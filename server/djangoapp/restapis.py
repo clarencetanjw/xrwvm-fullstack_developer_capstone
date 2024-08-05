7c8d0f9347a70f13a18261302bf344fb2f5804c7
clarencetanjw <clarencetan91@gmail.com>
2024-08-05 03:42:21 -0400

    Py Lint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -7,7 +7,10 @@ load_dotenv()
 
 # Load URLs from environment variables
 backend_url = os.getenv('BACKEND_URL', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('SENTIMENT_ANALYZER_URL', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv(
+    'SENTIMENT_ANALYZER_URL', default="http://localhost:5050/"
+)
+
 
 def get_request(endpoint, **kwargs):
     params = "&".join(f"{key}={value}" for key, value in kwargs.items())
@@ -20,6 +23,7 @@ def get_request(endpoint, **kwargs):
     except requests.exceptions.RequestException as e:
         print(f"Network exception occurred: {e}")
 
+
 def analyze_review_sentiments(text):
     encoded_text = quote(text)
     request_url = f"{sentiment_analyzer_url}/analyze/{encoded_text}"
@@ -31,6 +35,7 @@ def analyze_review_sentiments(text):
     except requests.exceptions.RequestException as e:
         print(f"Unexpected error occurred: {e}")
         return None
+
 
 def post_review(data_dict):
     request_url = f"{backend_url}/insert_review"
