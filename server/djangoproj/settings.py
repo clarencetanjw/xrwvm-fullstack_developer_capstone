560ac7d5dafb7a1fd8508fd12adcf076481ad5d5
clarencetanjw <clarencetan91@gmail.com>
2024-08-05 05:29:43 -0400

    Revised to Py Lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
@@ -10,6 +11,7 @@ SECRET_KEY = (
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = [
     'localhost',
