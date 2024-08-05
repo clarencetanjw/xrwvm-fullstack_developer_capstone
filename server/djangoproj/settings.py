35f48b2144a28d40b8c6ca8289bb95db1af79f53
clarencetanjw <clarencetan91@gmail.com>
2024-08-05 05:33:07 -0400

    Revised to Py Lint Requirements

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -16,15 +16,15 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     (
-        'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
+        'https://clarencetan9-8000.'
+        'theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     (
-        'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-        'proxy.cognitiveclass.ai'
+        'https://clarencetan9-8000.'
+        'theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ),
 ]
 
