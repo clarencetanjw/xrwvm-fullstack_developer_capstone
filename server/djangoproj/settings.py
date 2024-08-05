eb6e55c6522f72d278fa0ce8e8c7e28c3460784d
clarencetanjw <clarencetan91@gmail.com>
2024-08-05 05:28:20 -0400

    Revised to Py Lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -1,10 +1,29 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = (
+    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 ALLOWED_HOSTS = [
     'localhost',
-    'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://clarencetan9-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    ),
 ]
 
 TEMPLATES = [
